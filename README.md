86afba4da2439829a4a8c7952a7162e3b88ad8c0
Jorge Castillo <j.a.castillo.jc@gmail.com>
2024-07-10 17:04:49 -0300

    initial react vite config

---
+++ README.md
@@ -0,0 +1,11 @@
+# React + TypeScript + Vite
+
+# Instructions
+
+Clone this repository into **wp-content/plugins**, then, in the `React Vite + Typescript` folder run `npm install`.
+
+Activate the plugin and create a page, on the page add a html block with `<div id="my-react-app"></div>`.
+
+Need Node v.18.18.2
+
+Then run `npm run build` to update assets or `npm run watch` to develop.
---
+++ vite.config.ts
@@ -0,0 +1,55 @@
+import { defineConfig, loadEnv } from "vite";
+import path from "path";
+import react from "@vitejs/plugin-react";
+
+export default({ mode }) => {
+  process.env = {...process.env, ...loadEnv(mode, process.cwd())};
+  return defineConfig({
+    plugins: [react({ include: /\.(mdx|js|jsx|ts|tsx)$/ })],
+    root: path.join(__dirname, "src"),
+    base: `${process.env.VITE_BASE_URL}${process.env.VITE_BASE_PUBLIC}`,
+    resolve: {
+      alias: [
+        { find: '@', replacement: path.resolve(__dirname, 'src') }
+      ]
+    },
+  build: {
+        outDir: path.join(__dirname, "public"),
+        manifest: true,
+        rollupOptions: {
+          input: path.join(__dirname, "src/main.tsx"),
+          output: {
+            entryFileNames: "assets/[name].js",
+            chunkFileNames: "assets/[name].js",
+            assetFileNames: ({name}) => {
+              if (/\.(gif|jpe?g|png|svg)$/.test(name ?? '')){
+                return 'assets/images/[name].[ext]';
+            }
+            return 'assets/[name].[ext]';
+            },
+          },
+        },
+      },
+    esbuild: { loader: "tsx", include: /src\/.*\.tsx?$/, exclude: [] },
+    optimizeDeps: {
+      esbuildOptions: {
+        loader: {
+          ".js": "tsx"
+        },
+        plugins: [
+          {
+            name: "load-js-files-as-tsx",
+            setup(build) {
+              build.onLoad({ filter: /src\/.*\.js$/ }, async (args) => {
+                return {
+                  loader: "tsx",
+                  contents: await fs.readFile(args.path, "utf8"),
+                };
+              });
+            },
+          },
+        ],
+      },
+    },
+  })
+};
